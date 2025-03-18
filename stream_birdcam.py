df3773ed3822ba8c610abe0384ab47a36b5523f9
EJB <57454304+ehrenbentz@users.noreply.github.com>
2025-03-18 10:08:36 -0400

    Delete stream_birdcam.py

--- stream_birdcam.py
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Start a new screen session and run birdcam.py
-#screen -dmS birdcam bash -c 'python3 birdcam.py'
-
-# Start ngrok
-ngrok http 5000 --url wildlifecam.ngrok.io
-#ngrok http 5000 --basic-auth "birdcam:Preston1" --url wildlifecam.ngrok.io
